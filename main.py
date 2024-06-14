b50e1b78be824bc7221d06537897fdd1634adacc
Oli R <oliwierrobaszynski@gmail.com>
2024-06-14 16:05:41 +0200

    gra

---
+++ main.py
@@ -0,0 +1,30 @@
+import keyboard
+import os
+from time import sleep
+clear = lambda: os.system('cls')
+clear()
+print("Witaj w grze tylko na konsole")
+sleep(1)
+clear()
+d1 = "############################"
+d2 = "#                          #"
+d3 = "#                          #"
+d4 = "#                          #"
+d5 = "#                          #"
+d6 = "#                          #"
+d7 = "#                          #"
+d8 = "############################"
+
+
+while True:
+    print(d1)
+    print(d2)
+    print(d3)
+    print(d4)
+    print(d5)
+    print(d6)
+    print(d7)
+    print(d8)
+
+    sleep(0.1)
+    clear()
